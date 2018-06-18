75c66a81a4b247c6568522ad0b2097b6220a8d04
JackMarx <seriousfools@gmail.com>
2018-06-18 18:27:02 -0500

    initial commit

---
+++ public/index.js
@@ -0,0 +1,26 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Let's get started with Vue.js!",
+      message2: "Let me go to the page too!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
